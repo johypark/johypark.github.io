440662d37465c0a3ed7d0cc88bf0e09c06e67630
Joohyung Park <johy@dabyeol.com>
2022-08-14 21:38:44 +0900

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,5 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const container = document.querySelector(".container");
+container.style.backgroundImage = `url("img/${chosenImage}")`;
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "6bf6b809e40e15ac6c9e09051d8e9364";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  console.log("Can't fint you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
